779204652fdfcfc690b353055e18aa2e1e3d6a27
Sontus Chandra Anik <info.sontus@gmail.com>
2024-11-04 11:36:25 +0600

    Package Stracture Added

---
+++ packages/sontus/contractform/src/ContractFormServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sontus\ContractForm;
+use Illuminate\Support\ServiceProvider;
+
+class ContractFormServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+
+    }
+}
